2d102f791f51518f4d84b8d8844ec382aa40b722
franco arcieri <franco.arcieri@uniroma2.eu>
2022-05-13 17:07:23 +0200

    Aggiungere i file di progetto.

---
+++ Prodotto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        private static readonly HashSet<string> nomi = new HashSet<string>();
+        public int Codice { get; }
+
+        private string nome;
+        public string Nome {
+            get { return nome; }
+            set { if (nomi.Contains(value))
+                    throw new Exception("Product name already exists");
+                nomi.Add(value);
+                nome = value;
+            }
+        }
+        public string? Descrizione { get; set; }
+
+        public double Prezzo { get; set; }
+
+        private double iva;
+        public double Iva
+        {
+            get { return iva; }
+            set { iva = value; }
+        }
+
+        private Random myRand;
+
+        public Prodotto(string nome, string descrizione, double prezzo, double iva)
+        {
+            myRand = new Random();
+            this.Nome = nome;
+            this.Descrizione = descrizione;
+            this.Prezzo = prezzo;
+            this.Iva = iva;
+            Codice = myRand.Next(); //ma se il codice è uguale a altri codici già creat??
+        }
+
+        public double GetPrezzoIvato()
+        {
+            return Prezzo * (1.0 + Iva);
+        }
+
+        public string NomeEsteso()
+        {
+            return String.Format("{0}-{1}", Codice, Nome);
+        }
+        public string GetCodiceFormatted()
+        {
+            string scod = Codice.ToString();
+            if (scod.Length < 13)
+                return new String('0', 13 - scod.Length) + scod;
+            else
+                return scod;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using csharp_oop_shop;
+
+// See https://aka.ms/new-console-template for more information
+
+
+//Esercizio di oggi: C# Shop nome repo: csharp-oop-shop Nel progetto
+//csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti
+//dello shop. Un prodotto è caratterizzato da: - codice (numero intero) -
+//nome - descrizione - prezzo - iva.
+//Usate opportunamente i livelli di
+//accesso (public, private), i costruttori, i metodi getter e setter ed
+//eventuali altri metodi di “utilità” per fare in modo che:
+//- alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+//- Il codice prodotto sia accessibile solo in lettura
+//- Gli altri attributi siano accessibili sia in lettura che in scrittura
+//- Il prodotto esponga sia un metodo per avere il prezzo base
+//  che uno per avere il prezzo comprensivo di iva
+//- Il prodotto esponga un metodo per avere il nome esteso,
+//  ottenuto concatenando codice + nome
+//
+//Nel vostro programma principale, testate tutte le funzionalità della classe Prodotto.
+//
+//BONUS: create un metodo che restituisca il codice con un pad left di
+//0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091,
+//mentre codice 123445567 resta come è)
+
+//Console.WriteLine("Hello, World!");
+//Random myrand = new Random();
+//Console.WriteLine(myrand.Next());
+
+Prodotto pr1 = new Prodotto("Lampada", "lampada antica, due luci", 100, 0.22);
+Prodotto pr2 = new Prodotto("Sedia", "Sedia per sedute", 2000, 0.22);
+//Prodotto pr3 = new Prodotto("Lampada", "lampada antica, due luci", 100, 0.22);
+
+Console.WriteLine("PR1: {0}, {1}, {2}", pr1.NomeEsteso(), pr1.Codice, pr1.GetCodiceFormatted());
+
